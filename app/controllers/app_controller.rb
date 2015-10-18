30056f5a50b4dc890e62a636cf473e42a16ee0c5
mckshreder <garrettmckenzie@ymail.com>
2015-10-17 23:40:25 -0700

    just added rss parser

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -1,4 +1,19 @@
 class AppController < ApplicationController
   def index
   end
+
+  def rsstest
+  	require 'rubygems'
+	require 'simple-rss'
+	require 'open-uri'
+
+	@rss = SimpleRSS.parse open('http://www.4sgm.com/is-bin/INTERSHOP.enfinity/WFS/4sgm-Storefront-Site/en_US/-/USD/ViewParametricSearch-RSS;pgid=8uKCiKaqQORSR00pmU_Mlavu0000K_PVledH?SearchCategoryUUID=t_DAwGQTQFQAAAELiFM0E4U1&rsstitle=Baby+Items')
+
+  	 # url = "http://www.4sgm.com/is-bin/INTERSHOP.enfinity/WFS/4sgm-Storefront-Site/en_US/-/USD/ViewParametricSearch-RSS;pgid=8uKCiKaqQORSR00pmU_Mlavu0000K_PVledH?SearchCategoryUUID=t_DAwGQTQFQAAAELiFM0E4U1&rsstitle=Baby+Items"
+
+    # @feed = Feedjira::Feed.fetch_and_parse url
+
+
+  end
+
 end
